d294addb349f8e595a65dc0f282820d1ecb98c6b
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-01-20 11:13:36 +0530

    Type and Variable Diffreence

---
+++ Day1/HelloWorld/Java/TypeDifferentTest.java
@@ -0,0 +1,23 @@
+public class TypeDifferentTest
+{
+
+
+public static void main(String[]args)
+{
+	int age=10;
+	float height = 5.5f;
+	char sortstartingpoint='a';
+	boolean isAdult=true;
+	double distance = 1000005;
+	long phoneNumber= 9311616298l;
+	System.out.println(age);
+	System.out.println(height);
+	System.out.println(isAdult);
+	System.out.println(distance);
+	System.out.println(phoneNumber);
+	
+	
+
+}
+
+}
---
+++ Day1/HelloWorld/Java/TypingDifferenceByTeam.java
@@ -0,0 +1,26 @@
+public class TypingDifferenceByTeam
+{
+
+
+public static void main(String[]args)
+{
+	int age=10;
+
+	age="Deepak";
+	System.out.println(age);
+	float height = 5.5f;
+	char sortstartingpoint='a';
+	boolean isAdult=true;
+	double distance = 1000005;
+	long phoneNumber= 9311616298l;
+	System.out.println(age);
+	System.out.println(height);
+	System.out.println(isAdult);
+	System.out.println(distance);
+	System.out.println(phoneNumber);
+	
+	
+
+}
+
+}
