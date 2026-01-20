beedad962a9d8313cf9447c9e0d9728bcca616ea
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-01-20 18:02:46 +0530

    Assignments

---
+++ Handonlabs/Day.py
@@ -0,0 +1,24 @@
+# # print("Deepak Kumar")
+# # print("90332433333")
+# # print("deepakbajaj79@gmail.com")
+
+# ##print("Deepak Kumar \n 90332433333 \n deepakbajaj79@gmail.com")
+# name = "Deepak Kumar"
+# print(name)
+# phone = "90332433333"
+
+# email = "deepakbajaj79@gmail.com"
+
+# print(name,"\n", phone, "\n", email)
+
+# ##print("Deepak Kumar \n 90332433333 \n deepakbajaj79@gmail.com")
+name = "Deepak Kumar"
+print(name)
+name= "90332433333"
+print(name)
+name = "deepakbajaj79@gmail.com"
+print(name)
+
+
+
+
